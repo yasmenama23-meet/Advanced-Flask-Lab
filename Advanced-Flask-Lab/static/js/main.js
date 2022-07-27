1266eff1bc757a9afa9f885bad398fc964000c79
yasmena masalha <109899488+yasmenama23-meet@users.noreply.github.com>
2022-07-27 14:08:38 +0300

    Update main.js

--- Advanced-Flask-Lab/static/js/main.js
+++ Advanced-Flask-Lab/static/js/main.js
@@ -1,17 +1,11 @@
-/*
-	Visualize by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
 $(function() {
 
-	// Vars.
+	
 		var	$window = $(window),
 			$body = $('body'),
 			$wrapper = $('#wrapper');
 
-	// Breakpoints.
+	
 		skel.breakpoints({
 			xlarge:	'(max-width: 1680px)',
 			large:	'(max-width: 1280px)',
@@ -20,14 +14,14 @@ $(function() {
 			xsmall:	'(max-width: 480px)'
 		});
 
-	// Disable animations/transitions until everything's loaded.
+	
 		$body.addClass('is-loading');
 
 		$window.on('load', function() {
 			$body.removeClass('is-loading');
 		});
 
-	// Poptrox.
+	
 		$window.on('load', function() {
 
 			$('.thumbnails').poptrox({
